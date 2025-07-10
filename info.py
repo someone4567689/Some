0a012ff0df803e60fecab81ac84eedf2fcc85f80
Priyanshu1057 <99806982+Priyanshu1057@users.noreply.github.com>
2025-07-10 22:26:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,24 +107,22 @@ QUALITIES = [
     "HdRip",
     "web-dl",
     "bluray",
-    "hdr",
-    "fhd",
-    "240p",
+    
+    
+    
     "360p",
     "480p",
-    "540p",
+    
     "720p",
-    "960p",
+    
     "1080p",
-    "1440p",
-    "2K",
-    "2160p",
+    
     "4k",
-    "5K",
-    "8K",
+    
+    
 ]
 YEARS = [f"{i}" for i in range(2025, 2002, -1)]
-SEASONS = [f"season {i}" for i in range(1, 23)]
+SEASONS = [f"season {i}" for i in range(1, 10)]
 
 # Pictures And Reaction
 START_IMG = (
