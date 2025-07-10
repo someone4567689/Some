967c2dc518394266e4d35b0be3b8dfbf6b221a6e
Priyanshu1057 <99806982+Priyanshu1057@users.noreply.github.com>
2025-07-10 19:13:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://Priyanshu105777:03
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "jisshu")
 
 # Other Channel's
-SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "2824228432"))
+SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "0"))
 DELETE_CHANNELS = int(environ.get("DELETE_CHANNELS", "0"))
 request_channel = environ.get("REQUEST_CHANNEL", "0")
 REQUEST_CHANNEL = (
